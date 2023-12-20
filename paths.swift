82c46576742e67ad973257e8ffdcea861bf3a8f1
Morgan <2504532+mrgnw@users.noreply.github.com>
2023-12-20 03:22:38 -0700

    paths - zsh (dotfiles)
    should be zsh one-liner
    
    paths(){
    echo $PATH | tr ':' '\n' | uniq | sed 's/^/\t/'
    }

--- paths.swift
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env swift
-import Foundation
-print()
-if let path = ProcessInfo.processInfo.environment["PATH"] {
-    path.components(separatedBy: ":").forEach { print("\t\($0)") }
-}
-print()
